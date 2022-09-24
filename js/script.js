a3a7c7d84857e196c4a983f9f5683b5defc7e41f
Youssef Girgeis <youssef_girgeis@hotmail.com>
2022-09-23 20:32:14 -0400

    select random quote from quotes array

--- js/script.js
+++ js/script.js
@@ -1,33 +1,73 @@
-/******************************************
-Treehouse FSJS Techdegree:
-project 1 - A Random Quote Generator
-******************************************/
+/***
+ * `quotes` array
+ ***/
 
-// For assistance: 
-  // Check the "Project Resources" section of the project instructions
-  // Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
-
-/*** 
- * `quotes` array 
-***/
-
-
+const quotes = [
+  {
+    quote: 'Be yourself; everyone else is already taken.',
+    source: 'Oscar Wilde',
+    citation: '',
+    year: '',
+  },
+  {
+    quote:
+      "Two things are infinite: the universe and human stupidity; and I'm not sure about the universe.",
+    source: 'Albert Einstein',
+    citation: '',
+    year: '1905',
+  },
+  {
+    quote:
+      "You know you're in love when you can't fall asleep because reality is finally better than your dreams.",
+    source: 'Dr. Seuss',
+    citation: '',
+    year: '1950',
+  },
+  {
+    quote: "If you tell the truth, you don't have to remember anything.",
+    source: 'Mark Twain',
+    citation: '',
+    year: '1870',
+  },
+  {
+    quote:
+      'Live as if you were to die tomorrow. Learn as if you were to live forever.',
+    source: 'Mahatma Gandhi',
+    citation: '',
+    year: '1930',
+  },
+  {
+    quote:
+      "In three words I can sum up everything I've learned about life: it goes on.",
+    source: 'Robert Frost',
+    citation: '',
+    year: '1936',
+  },
+  {
+    quote: 'A friend is someone who knows all about you and still loves you.',
+    source: 'Elbert Hubbard',
+    citation: '',
+    year: '1900',
+  },
+];
 
 /***
  * `getRandomQuote` function
-***/
+ ***/
 
-
+const getRandomQuote = () => {
+  return quotes[Math.floor(Math.random() * quotes.length)];
+};
 
 /***
  * `printQuote` function
-***/
-
-
+ ***/
 
 /***
  * click event listener for the print quote button
  * DO NOT CHANGE THE CODE BELOW!!
-***/
+ ***/
 
-document.getElementById('load-quote').addEventListener("click", printQuote, false);
+// document
+//   .getElementById('load-quote')
+//   .addEventListener('click', printQuote, false);
