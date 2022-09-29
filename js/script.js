7d77b2cd56a9c0b4db9074075e68202bc981fa79
Youssef Girgeis <youssef_girgeis@hotmail.com>
2022-09-28 23:03:50 -0400

    add quotes in separate files

--- js/script.js
+++ js/script.js
@@ -2,55 +2,6 @@
  * `quotes` array
  ***/
 
-const quotes = [
-  {
-    quote: 'Be yourself; everyone else is already taken.',
-    source: 'Oscar Wilde',
-    citation: '',
-    year: '',
-  },
-  {
-    quote:
-      "Two things are infinite: the universe and human stupidity; and I'm not sure about the universe.",
-    source: 'Albert Einstein',
-    citation: '',
-    year: '1905',
-  },
-  {
-    quote:
-      "You know you're in love when you can't fall asleep because reality is finally better than your dreams.",
-    source: 'Dr. Seuss',
-    citation: '',
-    year: '1950',
-  },
-  {
-    quote: "If you tell the truth, you don't have to remember anything.",
-    source: 'Mark Twain',
-    citation: '',
-    year: '1870',
-  },
-  {
-    quote:
-      'Live as if you were to die tomorrow. Learn as if you were to live forever.',
-    source: 'Mahatma Gandhi',
-    citation: '',
-    year: '1930',
-  },
-  {
-    quote:
-      "In three words I can sum up everything I've learned about life: it goes on.",
-    source: 'Robert Frost',
-    citation: '',
-    year: '1936',
-  },
-  {
-    quote: 'A friend is someone who knows all about you and still loves you.',
-    source: 'Elbert Hubbard',
-    citation: '',
-    year: '1900',
-  },
-];
-
 /***
  * `getRandomQuote` function
  ***/
